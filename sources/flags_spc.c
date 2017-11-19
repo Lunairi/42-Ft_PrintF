e64b6702479c756385fc53d84cbd505f250440ea
Lunairi <mizukori250@gmail.com>
2017-11-18 16:31:26 -0800

    hail  mary to try and get plus working

--- sources/flags_spc.c
+++ sources/flags_spc.c
@@ -57,7 +57,7 @@ void	flags_plus(va_list args, int *p, const char *str, int *i)
 		parse_leftpalign(i, str, args, p);
 	else if (str[*i + 1] > '0' || str[*i + 1] <= '9')
 		parse_rightpalign(i, str, args, p);
-	else if (str[*i] == '0')
+	else if (str[*i + 1] == '0')
 	{
 		*p = *p + 1;
 		write(1, "+", 1);
